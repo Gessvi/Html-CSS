101841af904234103c94cb2af50356ab009fb438
Gessica Vasconcelos <143418965+Gessvi@users.noreply.github.com>
2024-08-17 17:39:01 -0300

    atualizei cadastro

---
+++ Exercicios/Desafios/tcc/testeLogin.php
@@ -0,0 +1,13 @@
+<?php
+
+//print_r($_REQUEST);
+
+if(isset($_post['submit']) && !empty($_post['email']) && !empty($_post[senha]))
+{
+    include_once('')
+}
+else
+{
+    header('Location: login.php');
+}
+?>
